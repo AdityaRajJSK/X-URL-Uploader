d16e9c0381f5851e17d9b7ffe6e60d26c586d927
AdityaRajJSK <73116892+AdityaRajJSK@users.noreply.github.com>
2023-05-14 18:39:35 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -213,8 +213,12 @@ async def echo(bot, update):
             if "\n" in x_reponse:
                 x_reponse, _ = x_reponse.split("\n")
             response_json = json.loads(x_reponse)
+            
+            if not os.path.isdir(Config.DOWNLOAD_LOCATION + \
+                "/" + str(update.from_user.id)):
             os.mkdir(Config.DOWNLOAD_LOCATION + \
                 "/" + str(update.from_user.id))
+            
             save_ytdl_json_path = Config.DOWNLOAD_LOCATION + \
                 "/" + str(update.from_user.id) + "/" + file_name.replace(".mp4", "") + ".json"
             with open(save_ytdl_json_path, "w", encoding="utf8") as outfile:
