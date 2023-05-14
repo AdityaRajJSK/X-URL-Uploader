d008fe2a2d34feda370a021a2d78312f64b0a204
AdityaRajJSK <73116892+AdityaRajJSK@users.noreply.github.com>
2023-05-14 15:11:52 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -249,7 +249,7 @@ async def youtube_dl_call_back(bot, update):
             else:
                 video_input_path=download_directory
                 img_output_path = Config.DOWNLOAD_LOCATION + \
-        "/" + str(update.from_user.id) + "/thumb.jpg"
+        "/" + str(update.from_user.id) + custom_file_name.replace(".mp4", "") + "/thumb.jpg"
                 subprocess.call(['ffmpeg', '-i', video_input_path, '-ss', '00:00:02.000', '-vframes', '1', img_output_path])
                 thumb_image_path=img_output_path
                 width = 0
